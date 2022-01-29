72d085e49cec17c317a3aa46ecb5199d1b5f4e8c
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-01-29 17:17:44 +0300

    bug fix get_total_pages function

--- main.py
+++ main.py
@@ -13,22 +13,38 @@ def get_html(url=URL):
 
 def get_total_pages(html_code):
     soup = BeautifulSoup(html_code, 'lxml')
-    pages = (
-        soup.find(
-            'div', class_='pagination-root-Ntd_O'
-        ).find_all('span', class_='pagination-item-JJq_j')[-2].get('data-marker')
-    )
-    total_pages = int(pages.split('(')[-1].split(')')[0])
+    pages = soup.find('div', class_='pagination-pages clearfix').find_all('a', class_='pagination-page')[-1]
+    total_pages = int(pages.get('href').split('=')[-1])
     return total_pages
 
+
+def html_data(html_text):
+    soup = BeautifulSoup(html_text, 'lxml')
+    links = soup.find_all('div', class_='iva-item-content-rejJg')
+    # title, price, metro, url
+    for link in links:
+        title = link.find('h3').text
+        price = link.find('span', class_='price-text-_YGDY text-text-LurtD text-size-s-BxGpL').text
+        metro = link.find('div', class_='geo-georeferences-SEtee text-text-LurtD text-size-s-BxGpL').text
+        url = link.find('a').get('href')
+        print(f'{title} - title')
+        print(f'{price} - price')
+        print(f'{metro}')
+        print(f'{url} - url\n')
+        # try:
+        #     title =
 
 def main():
     resp = get_html()
     get_total_pages(resp)
-    count_links = get_total_pages(resp) + 1
-    for i in range(1, count_links):
-        gen_url = URL + str(i)
-        print(gen_url)
+    # count_links = get_total_pages(resp) + 1
+    # for i in range(1, 4):
+    #     gen_url = URL + str(i)
+    #     page_html = get_html(gen_url)
+    #     html_data(page_html)
+    # gen_url = URL + '4'
+    # page_html = get_html(gen_url)
+    # html_data(page_html)
 
 
 if __name__ == '__main__':
