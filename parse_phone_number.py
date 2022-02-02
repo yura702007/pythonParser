4b48852e4697a22387f50856451b5c0fc0a43403
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-02-02 22:07:25 +0300

    add geckodriver

---
+++ parse_phone_number.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+URL = 'https://www.avito.ru/moskva/telefony/samsung_galaxy_a52_8256gb_black_ru_2305099331'
+
+
+class Bot:
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
