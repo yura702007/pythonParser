c3c5b2256b46dd66a63f9813430b52e2c5612151
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-02-04 14:09:04 +0300

    add proxy_list and user-agent_list

--- use_proxy.py
+++ use_proxy.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 URL = 'https://sitespy.ru/my-ip'
 
@@ -16,7 +17,26 @@ def get_ip(html_text):
     print(ip, '\n', ua)
 
 
+def get_proxy():
+    lst = []
+    with open('proxy_list.csv', 'r', newline='', encoding='utf8') as file:
+        reader = csv.DictReader(file, fieldnames=('ip', 'port'))
+        for row in reader:
+            lst.append(row)
+    return lst[1:]
+
+
+def get_ua():
+    lst = []
+    with open('user_agents.txt', 'r', encoding='utf8') as file:
+        for line in file:
+            lst.append(line.strip())
+    return lst
+
+
 def main():
+    list_proxy = get_proxy()
+    list_ua = get_ua()
     get_ip(get_html())
 
 
